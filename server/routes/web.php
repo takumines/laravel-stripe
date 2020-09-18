ce40a411be54d9c381a10308c3562bc4d8370901
takumi_kurogi <takumines1@gmail.com>
2020-09-18 16:24:47 +0900

    m[update]webhookのエンドポイント、コントローラを追加

--- server/routes/web.php
+++ server/routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::post('stripe/webhook', 'User\StripeWebhookController@index');
 
 Route::prefix('user')->middleware(['auth'])->group(function() {
     Route::get('subscription', 'User\SubscriptionController@index');
